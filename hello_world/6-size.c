aff8c7c61b6ee3b7cce918c1c53f020e5c3124a4
alfred <6666@holbertonstudents.com>
2023-05-25 15:05:14 -0700

    1..100

---
+++ hello_world/6-size.c
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo '#include <stdio.h> int main(void) { printf("Size of a char: %zu byte(s)\nSize of an int: %zu byte(s)\nSize of a long int: %zu byte(s)\nSize of a long long int: %zu byte(s)\nSize of a float: %zu byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long
