737f0cbac5f77c6f13ba5755f760e63f3f034483
alfred <6666@holbertonstudents.com>
2023-05-25 14:51:57 -0700

    1..100

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-int main(void) { return puts("Programming is like building a multilingual puzzle"), 0; }
+int main(void) { return puts("Programming is like building a multilingual puzzle\n") == EOF ? 1 : 0; }
