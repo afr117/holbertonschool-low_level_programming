8cffb8e35f6aca1860fb36f66b50c4618d158f3e
alfred <6666@holbertonstudents.com>
2023-05-29 12:29:13 -0700

    1..100

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo '#include <stdio.h>' > main.c && echo 'int main(void) { puts("Programming is like building a multilingual puzzle,"); return 0; }' >> main.c && gcc main.c -o cisfun
+echo 'Programming is like building a multilingual puzzle'
