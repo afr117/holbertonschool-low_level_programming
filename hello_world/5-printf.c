6efd036e3367a7157bc9d24933d1480e64b053ef
alfred <6666@holbertonstudents.com>
2023-05-30 09:00:09 -0700

    1..100

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-int main(void){return printf("with proper grammar, but the outcome is a piece of art,\n"),0;}
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void) {
+    printf("%s%s%s%s%s", "\"Programming is", " like ", "building a ", "multilingual puzzle", ", but the outcome is a piece of art,\"");
+    return 0;
+}
