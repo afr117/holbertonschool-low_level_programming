f64e9893d44d714599c4aa8b4a16e91ac46c1553
alfred <6666@holbertonstudents.com>
2023-05-31 13:16:28 -0700

    1..100

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,13 +4,14 @@
 
 /* betty style doc for function main goes there */
 int main(void)
-{
+{	
 	int n;
 	int = fox
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	fox = n % 10;
+	
 	if (fox > 10)
 	{
 	printf("Last digit of %d is %d and i is greater than 5\n", n fox);
