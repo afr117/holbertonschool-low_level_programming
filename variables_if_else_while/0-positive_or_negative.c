5d48721d6a1a4dae5a577d8ed2e845210ffe1a11
alfred <6666@holbertonstudents.com>
2023-05-30 21:12:57 -0700

    1..100

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,21 +11,23 @@
 int main(void)
 {
     int n;
-    
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    
+
+    srand(time(0));  // Seed the random number generator with the current time
+    n = rand() % 201 - 100;  // Generate a random number between -100 and 100
+
+    printf("%d is ", n);
     if (n > 0)
     {
-	printf("%d is positive\n");
+        printf("positive\n");
     }
     else if (n == 0)
     {
-	printf("%d is %s\n", n);
+        printf("zero\n");
     }
-    else ()
+    else
     {
-	printf("%d is %s\n", n)
+        printf("negative\n");
     }
 
-    return (0);
+    return 0;
+}
