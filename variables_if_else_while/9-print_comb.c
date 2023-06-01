ffa652300c9e96615dc88cd4151a2140900bd685
alfred <6666@holbertonstudents.com>
2023-05-31 21:08:05 -0700

    1..100

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+	putchar('0' + digit);
+	}
+
+	for (digit = 'a'; digit <= 'f'; digit++)
+	{
+	putchar(digit);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
