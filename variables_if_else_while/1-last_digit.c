01f1256ee5fd92811cde8069cc302416c9e1bbb9
alfred <6666@holbertonstudents.com>
2023-05-31 17:54:25 -0700

    1..100

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,9 +4,9 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 int main(void)
-{	
+{
 	int n;
-	int () = fox;
+	int = fox;
 
 	srand(time(0));
     n = rand() - RAND_MAX / 2;
