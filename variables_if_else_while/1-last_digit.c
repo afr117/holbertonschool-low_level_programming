44c7984cc128b7476c572f2af97cb9c716cc2977
alfred <6666@holbertonstudents.com>
2023-05-31 18:21:33 -0700

    1..100

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,21 +6,19 @@
 int main(void)
 {
 	int n;
-	int = fox;
+	int fox;
 
 	srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    fox = n % 10;
+	n = rand() - RAND_MAX / 2;
+	fox = n % 10;
 
-    printf("Last digit of %d is %d and ", n, fox);
+	printf("Last digit of %d is %d and ", n, fox);
 
-    if (fox > 5) {
-        printf("is greater than 5\n");
-    } else if (fox == 0) {
-        printf("is 0\n");
-    } else {
-        printf("is less than 6 and not 0\n");
-    }
-
-    return 0;
+	if (fox > 5)
+	printf("is greater than 5\n");
+	else if (fox == 0)
+	printf("is 0\n");
+	else
+	printf("is less than 6 and not 0\n");
+	return (0);
 }
