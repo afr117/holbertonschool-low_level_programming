7584e6e7ba0a6a63b0b8a74118769a3937ed4b82
alfred <6666@holbertonstudents.com>
2023-05-31 19:03:29 -0700

    1..100

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	printlank line
+
+	return (0);
+}
