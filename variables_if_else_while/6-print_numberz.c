342f924949878cbe986729c9969da2b869b69d9f
alfred <6666@holbertonstudents.com>
2023-05-31 20:41:21 -0700

    1..100

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -11,12 +11,12 @@ int main(void)
 {
 	char digit;
     
-	for (digit = '0'; digit <= '9'; digit++)
+	for (digit = 0 ; digit <= '9'; digit++)
 	{
 		putchar('0' + digit);
 	}
 
-	putchar("\n");
+	putchar('\n');
 
 	return 0;
 }
