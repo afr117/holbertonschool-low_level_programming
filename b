953a4c0c36791b005360c2e95da8a0c2a08b4033
alfred <6666@holbertonstudents.com>
2023-06-01 17:43:50 -0700

    1..1000

---
+++ b
@@ -0,0 +1,4 @@
+#!/bin/bash 
+git add .
+git commit -m "1..1000"
+git push
---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+		char a = 1;
+	if (a == 1)
+		printf("_putchar\n");
+	return(0);
+}
