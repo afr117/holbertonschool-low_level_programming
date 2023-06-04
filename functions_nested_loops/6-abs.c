54becb77bd376a8040590fd873da1eb10781c082
alfred <6666@holbertonstudents.com>
2023-06-04 15:58:26 -0700

    1..1000

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,15 +1,19 @@
 #include "main.h"
+#include "5-sign.c"
+#include <stdio.h>
 
 /**
  * This function computes the absolute value of an integer.
  */
 
-int _abs(int num)
+int _abs(int num);
 {
 	if (num < 0)
-        {
-        return -num;
-        } else {
-        return num;
-        }
+	{
+	return (-num);
+	}
+	else
+	{
+	return (num);
+	}
 }
