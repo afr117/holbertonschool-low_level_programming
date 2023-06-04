281b38b0cdc4f0ac7ca6713fb48d8dace544507f
alfred <6666@holbertonstudents.com>
2023-06-04 11:41:35 -0700

    1..1000

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,6 +1,20 @@
-#ifndef PRINT_SIGN_H
-#define PRINT_SIGN_H
+#include "main.h"
 
-int print_sign(int n);
-
-#endif /* PRINT_SIGN_H */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return 1;
+	}
+	else if (n == 0)
+	{
+	_putchar('0');
+	return 0;
+	}
+	else
+	{
+	_putchar('-');
+	return -1;
+	}
+}
