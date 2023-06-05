f75558b22d3c4a25f1391ecce9ddd7f376111590
alfred <6666@holbertonstudents.com>
2023-06-04 18:02:41 -0700

    1..1000

--- functions_nested_loops/7-main.c
+++ functions_nested_loops/7-main.c
@@ -1,10 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+int print_last_digit(int num)
+{
+	int last_digit = num % 10;
+	_putchar('0' + last_digit);
+	return last_digit;
+}
+
 int main(void)
 {
 	int r;
