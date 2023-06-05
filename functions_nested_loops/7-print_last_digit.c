bc2fdb8d44c53d575aabaf5f454c1f7d47838ef9
alfred <6666@holbertonstudents.com>
2023-06-05 09:06:44 -0700

    1..1000

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,16 +10,9 @@
  *
  * Get the last digit using the modulus operator
  */
-int print_last_digit(int num)
-{
-	int last_digit = num % 10;
-	printf("The last digit is: %d\n", last_digit);
-	return last_digit;
-}
-
 int main()
 {
-	int number = 12345;
-	print_last_digit(number);
-	return (0);
+        int number = 12345;
+        print_last_digit(number);
+        return (0);
 }
