d27f24da6e0bdb0a40a652488be7b77c0bbe04fc
alfred <6666@holbertonstudents.com>
2023-06-04 17:20:30 -0700

    1..1000

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_last_digit - Prints the last digit of a number and returns its value.
+ *
+ * @num: The input number.
+ *
+ * Return: The value of the last digit.
+ *
+ * Get the last digit using the modulus operator
+ */
+int print_last_digit(int num)
+{
+	int last_digit = num % 10;
+
+	printf("The last digit is: %d\n", last_digit);
+
+	return (last_digit);
+}
+
+int main()
+{
+	int number = 12345;
+	int last_digit = print_last_digit(number);
+
+	return (0);
+}
