542b7e1a8e161371945b710ffb8ca2254a143140
alfred <6666@holbertonstudents.com>
2023-06-05 14:13:07 -0700

    1..1000

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * Function: print_last_digit
- *
- * Description:
- *
- * This function takes an integer 'num' as input and returns its last digit.
- *
- * The function also prints the last digit.
- *
- * Parameters:
- * - num: An integer number from which the last digit needs to be extracted.
- * Returns:
- The last digit of the input number 'num'.
+/*
+   Function: print_last_digit
+
+   Description:
+      This function takes an integer 'num' as input and returns its last digit.
+      The function also prints the last digit.
+
+   Parameters:
+      - num: An integer number from which the last digit needs to be extracted.
+
+   Returns:
+      The last digit of the input number.
 */
 int print_last_digit(int num)
 {
