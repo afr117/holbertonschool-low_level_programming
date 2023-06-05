86fcfe67bc9e585eefb1c4af8f2d2bfa66436e3f
alfred <6666@holbertonstudents.com>
2023-06-05 16:10:00 -0700

    1..1000

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,16 +11,21 @@ void times_table(void)
 	{
 	for (column = 0; column <= 9; column++)
 	{
-		product = row * column;
+	product = row * column;
 	if (column != 0)
 	{
-		_putchar(',');
+	_putchar(',');
+	_putchar(' ');
 	if (product < 10)
-	_putchar(' ');
-	_putchar(' ');
+	_putchar(' ');            } 
+	if (product < 10)
+	_putchar(product + '0');
+	else
+	{
+	_putchar((product / 10) + '0');
+	_putchar((product % 10) + '0');
 	}
-		_putchar(product + '0');
 	}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
