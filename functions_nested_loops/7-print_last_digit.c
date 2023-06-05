7f4baee3a02f68e8fab533d522aa8d60ba790a07
alfred <6666@holbertonstudents.com>
2023-06-05 10:27:45 -0700

    1..1000

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,11 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * print_last_digit - Prints the last digit of a number and returns its value.
- * @num: The input number.
- * Return: The value of the last digit.
- * Get the last digit using the modulus operator
+/* Calculate the remainder when divided by 10
  */
-int print_last_digit(int num)
-{
-	int last_digit = num % 10;
-	printf("The last digit is: %d\n", last_digit);
-	return last_digit;
+int print_last_digit(int num) {
+    int last_digit = num % 10;
+    if (num < 0)
+	num = -num;
+
+    return last_digit;
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,7 @@
 int _putchar(char c);
+
+int _abs(int);
+
 int print_sign(int n);
+
 int print_last_digit(int num);
