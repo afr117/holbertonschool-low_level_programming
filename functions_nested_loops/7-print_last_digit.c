9d36f6e0a496fe2743725b73199edb6396d146ef
alfred <6666@holbertonstudents.com>
2023-06-05 14:00:50 -0700

    1..1000

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 
-/* Calculate the remainder when divided by 10
+/**
+ * Calculate the remainder when divided by 10
  */
-int print_last_digit(int num) {
+int print_last_digit(int num)
+{
 	int last_digit = num % 10;
 	if (last_digit < 0)
 		last_digit = -last_digit;
 	_putchar (last_digit + '0');
 
-	return last_digit;
+	return (last_digit);
 }
