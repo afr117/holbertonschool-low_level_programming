114ed3bb75f1102270264c96917e47c4cf9431b1
alfred <6666@holbertonstudents.com>
2023-06-05 16:03:18 -0700

    1..1000

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table.
+ */
+void times_table(void)
+{
+	int row, column, product;
+
+	for (row = 0; row <= 9; row++)
+	{
+	for (column = 0; column <= 9; column++)
+	{
+		product = row * column;
+	if (column != 0)
+	{
+		_putchar(',');
+	if (product < 10)
+	_putchar(' ');
+	_putchar(' ');
+	}
+		_putchar(product + '0');
+	}
+		_putchar('\n');
+	}
+}
