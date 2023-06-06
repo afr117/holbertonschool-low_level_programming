0e1fc6172d3b8ccbdfd4269a235cdb16b95e5181
alfred <6666@holbertonstudents.com>
2023-06-06 13:44:42 -0700

    1 .. 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -14,13 +14,12 @@ int _atoi(char *s)
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == '-')
-			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-			result = (result * 10) + (s[i] - '0');
-		else if (result != 0)
-			break;
-		i++;
+	if (s[i] == '-')
+	sign *= -1;
+	else if (s[i] >= '0' && s[i] <= '9')			result = (result * 10) + (s[i] - '0');
+	else if (result != 0)
+	break;
+	i++;
 	}
 	return (result * sign);
 }
