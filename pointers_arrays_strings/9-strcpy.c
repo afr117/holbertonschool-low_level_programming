0c7c736fe2ce6fa3aedd4c602e13497129fd281c
alfred <6666@holbertonstudents.com>
2023-06-06 13:17:11 -0700

    1 .. 10

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _putchar - Writes a character to stdout.
+ * @c: The character to be written.
+ *
+ * Return: On success, the character written. On error, -1 is returned.
+ */
+int _putchar(char c);
+
+/**
+ * _strcpy - Copies the string pointed to by src, including the terminating null byte,
+ *            to the buffer pointed to by dest.
+ * @dest: Pointer to the destination buffer.
+ * @src: Pointer to the source string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	dest[i] = src[i];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
