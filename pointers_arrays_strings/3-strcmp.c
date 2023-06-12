967d6d25a1b21e98552e79ad8395e0e20f0687ba
alfred <6666@holbertonstudents.com>
2023-06-11 19:39:58 -0700

    1 .. 10

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: Pointer to the first string.
+ * @s2: Pointer to the second string.
+ *
+ * Return:
+ *   < 0 if s1 is less than s2.
+ *   0 if s1 is equal to s2.
+ *   > 0 if s1 is greater than s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+    while (*s1 && (*s1 == *s2))
+    {
+        s1++;
+        s2++;
+    }
+
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
