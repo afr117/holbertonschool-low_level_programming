47c1030e614e0e5c148d6f2e2ffe9bb9e24b49d3
alfred <6666@holbertonstudents.com>
2023-06-11 20:18:15 -0700

    1 .. 10

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+char *leet(char *str)
+{
+	char *letters = "aAeEoOtTlL";
+	char *leet_nums = "4433007711";
+	int i, j;
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	for (j = 0; letters[j] != '\0'; j++)
+	{
+	if (str[i] == letters[j])
+	{
+	str[i] = leet_nums[j];
+	break;
+	}
+	}
+	} 
+
+	return str;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -35,3 +35,5 @@ char *string_toupper(char *);
 void reverse_array(int *a, int n);
 
 char *cap_string(char *);
+
+char *leet(char *);
