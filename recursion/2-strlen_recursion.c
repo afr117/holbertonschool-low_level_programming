08cf80b724a1207137f32d12f7728b9d34623f14
alfred <6666@holbertonstudents.com>
2023-06-22 13:16:51 -0700

    1..100

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -9,7 +9,7 @@
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0') /* Base case: end of string */
-		return 0;
+		return (0);
 
-	return 1 + _strlen_recursion(s + 1); /* Recursively count remaining characters */
+	return (1 + _strlen_recursion(s + 1)); /* Recursively count remaining characters */
 }
