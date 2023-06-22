021fa6f74712b999085e1e2f37f22492f74c4c9f
alfred <6666@holbertonstudents.com>
2023-06-22 12:41:37 -0700

    1..100

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+int _putchar(char c);
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: The string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0') /* Base case: end of string */
+		return;
+
+	_print_rev_recursion(s + 1); /* Recursively print remaining characters */
+	_putchar(*s); /* Print current character */
+}
