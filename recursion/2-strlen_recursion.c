158e78eed0f23763117d9305d1b3ef628b0a9522
alfred <6666@holbertonstudents.com>
2023-06-22 13:20:24 -0700

    1..100

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _strlen_recursion: Returns length of string
+ * _strlen_recursion - Returns length of string
  * @s: The string to measure
  *
  * Return: The length of the string
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0') /* Base case: end of string */
+	if (*s == '\0') /* Base case: string end */
 		return (0);
 
 	return (1 + _strlen_recursion(s + 1)); /* Recursively count remaining characters */
