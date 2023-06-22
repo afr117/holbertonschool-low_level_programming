e1c6a442f14c95e6a6aaafa6ba4e783c6ecc374b
alfred <6666@holbertonstudents.com>
2023-06-22 12:20:22 -0700

    1..100

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ *
+ * Return: On success 1, -1 on error.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _puts_recursion - Prints a string, followed by a new line
+ * @s: The string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0') /* Base case: end of string */
+	{
+		_putchar('\n'); /* Print new line */
+		return;
+	}
+
+	_putchar(*s); /* Print current character */
+	_puts_recursion(s + 1); /* Recursively print remaining characters */
+}
