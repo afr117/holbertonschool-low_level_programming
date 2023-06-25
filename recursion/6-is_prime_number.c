ec6e39f122603fb93e48e94b400a9c3243bed64e
alfred <6666@holbertonstudents.com>
2023-06-25 15:25:43 -0700

    1..100

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -18,10 +18,10 @@ int is_prime_helper(int n, int divisor);
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
+	if (n <= 1)
+	return (0);
 
-    return (is_prime_helper(n, 2));
+	return (is_prime_helper(n, 2));
 }
 
 /**
@@ -33,11 +33,11 @@ int is_prime_number(int n)
  */
 int is_prime_helper(int n, int divisor)
 {
-    if (divisor * divisor > n)
-        return (1);
+	if (divisor * divisor > n)
+	return (1);
 
-    if (n % divisor == 0)
-        return (0);
+	if (n % divisor == 0)
+	return (0);
 
-    return (is_prime_helper(n, divisor + 1));
+	return (is_prime_helper(n, divisor + 1));
 }
