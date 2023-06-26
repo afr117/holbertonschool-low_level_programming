1d7f8857a27480404bbfeccfaf8a6e9fcadb2418
alfred <6666@holbertonstudents.com>
2023-06-26 16:11:47 -0700

    1..100

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,27 +12,17 @@ int *array_range(int min, int max)
 	int *arr;
 	int size, i;
 
-	/* Check if min > max */
 	if (min > max)
-	{
 		return (NULL);
-	}
 
-	/* Calculate the size of the array */
 	size = max - min + 1;
 
-	/* Allocate memory for the array using malloc */
 	arr = malloc(size * sizeof(int));
 	if (arr == NULL)
-	{
 		return (NULL);
-	}
 
-	/* Fill the array with values from min to max */
 	for (i = 0; i < size; i++)
-	{
 		arr[i] = min++;
-	}
 
 	return (arr);
 }
