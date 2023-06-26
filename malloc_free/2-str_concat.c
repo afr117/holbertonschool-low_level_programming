5dd6e84476b7e6ecd04f9969d40d7d4286470ace
alfred <6666@holbertonstudents.com>
2023-06-26 09:52:57 -0700

    1..200

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,29 +10,29 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concat;
-    int len1 = 0, len2 = 0, i, j;
+	char *concat;
+	int len1 = 0, len2 = 0, i, j;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
-    while (s2[len2] != '\0')
-        len2++;
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-    concat = malloc((len1 + len2 + 1) * sizeof(char));
-    if (concat == NULL)
-        return (NULL);
+	concat = malloc((len1 + len2 + 1) * sizeof(char));
+	if (concat == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
-    for (j = 0; j < len2; j++)
-        concat[i++] = s2[j];
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
+	for (j = 0; j < len2; j++)
+		concat[i++] = s2[j];
 
-    concat[i] = '\0';
+	concat[i] = '\0';
 
-    return (concat);
+	return (concat);
 }
