2c39aeef05ae1d729edf5e4203feab020ac298df
alfred <6666@holbertonstudents.com>
2023-06-26 07:24:49 -0700

    1..200

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments passed
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: 0 on success
+ */
+
 int main(int argc, char *argv[])
 {
 	(void)argc; /* Ignore the unused parameter warning */
