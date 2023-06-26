c7a4978d72c8c0d2b466df377f9ace5a0c8bf61d
alfred <6666@holbertonstudents.com>
2023-06-26 07:19:46 -0700

    1..200

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    (void)argc; /* Ignore the unused parameter warning */
-    printf("%s\n", argv[0]);
-    return (0);
+int main(int argc, char *argv[])
+{
+	(void)argc; /* Ignore the unused parameter warning */
+
+	printf("%s\n", argv[0]);
+	return (0);
 }
