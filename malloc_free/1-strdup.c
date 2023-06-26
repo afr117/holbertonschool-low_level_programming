04b08c68172952498afd0233a7921bb2e7f21631
alfred <6666@holbertonstudents.com>
2023-06-26 09:20:10 -0700

    1..200

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,14 +3,18 @@
 
 char *_strdup(char *str)
 {
-	if (str == NULL)
-	return (NULL);
+	size_t length;
+	char *duplicate;
 
-	size_t length = strlen(str) + 1;
-	char *duplicate = malloc(length * sizeof(char));
+	if (str == NULL)
+	return NULL;
+
+	length = strlen(str) + 1;
+	duplicate = malloc(length * sizeof(char));
 
 	if (duplicate != NULL)
 	memcpy(duplicate, str, length);
 
 	return (duplicate);
 }
+
