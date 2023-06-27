070657ac33e60373cc9d60d3debf0603b3077e7e
alfred <6666@holbertonstudents.com>
2023-06-26 18:09:20 -0700

    1 .. 100

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the name of the file it was compiled from.
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
