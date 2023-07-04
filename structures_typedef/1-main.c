931dbb2884822a01218e6799ac2eb01a9c2b449e
alfred <6666@holbertonstudents.com>
2023-07-04 06:20:34 -0700

    1 .. 100

--- structures_typedef/1-main.c
+++ structures_typedef/1-main.c
@@ -5,15 +5,17 @@
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
-    struct dog my_dog;
+struct dog my_dog;
 
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
+	printf("My dog's name is %s, and it is %.2f years old\n", my_dog.name, my_dog.age);
+	printf("It is owned by %s\n", my_dog.owner);
+
+	return (0);
 }
