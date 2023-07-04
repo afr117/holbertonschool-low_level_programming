13e74272e875e9934e017aa396c918807ab2b186
alfred <6666@holbertonstudents.com>
2023-07-03 19:05:31 -0700

    1..100

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,9 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdio.h>
 
-/* Struct definition for dog */
-
+/*
+ * Struct definition for dog
+ */
 struct dog {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* DOG_H */
