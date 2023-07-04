fca581c52a1a2f63b130b0dfa2d97ac6f675d029
alfred <6666@holbertonstudents.com>
2023-07-04 12:41:35 -0700

    1..100

--- function_pointers/0-main.c
+++ function_pointers/0-main.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+void print_name_as_is(char *name)
+{
+    printf("Hello, my name is %s\n", name);
+}
+
+void print_name_uppercase(char *name)
+{
+    unsigned int i;
+
+    printf("Hello, my uppercase name is ");
+    i = 0;
+    while (name[i])
+    {
+        if (name[i] >= 'a' && name[i] <= 'z')
+        {
+            putchar(name[i] + 'A' - 'a');
+        }
+        else
+        {
+            putchar(name[i]);
+        }
+        i++;
+    }
+    printf("\n");
+}
+
 int main(void)
 {
-	void print_name_uppercase(char *name); /* Declare the function before usage */
+    print_name("Bob", print_name_as_is);
+    print_name("Bob Dylan", print_name_uppercase);
 
-	print_name("Bob", print_name_as_is);
-	print_name("Bob Dylan", print_name_uppercase);
-
-	return (0);
+    return (0);
 }
+
--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,34 +1,9 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-void print_name_as_is(char *name)
-{
-    printf("Hello, my name is %s\n", name);
-}
-
-void print_name_uppercase(char *name)
-{
-    unsigned int i;
-
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
-}
-
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
+    if (name && f) /* Check if name and f are not NULL */
         f(name);
 }
 
