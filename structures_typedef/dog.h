2146f5206cea39757c4c08f9afb0fe4306aa2647
alfred <6666@holbertonstudents.com>
2023-07-04 05:31:52 -0700

    1..100

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,16 +3,11 @@
 
 #include <stdio.h>
 
-/**
- * @struct dog
- * @brief Represents a dog with its name, age, and owner information.
- */
 struct dog
 {
-    char *name;     /**< Name of the dog */
-    float age;      /**< Age of the dog */
-    char *owner;    /**< Owner of the dog */
+        char *name;     /* Name of the dog */
+        float age;      /* Age of the dog */
+        char *owner;    /* Owner of the dog */
 };
 
 #endif /* DOG_H */
-
