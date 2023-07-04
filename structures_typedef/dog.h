363d0ab086803539eedaa50d5d549cf61808b98f
alfred <6666@holbertonstudents.com>
2023-07-04 06:49:03 -0700

    1 .. 100

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,5 +21,6 @@ struct dog
 
 /* Function prototype for init_dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+/* Function prototype for print_dog */
+void print_dog(struct dog *d);
 #endif /* DOG_H */
