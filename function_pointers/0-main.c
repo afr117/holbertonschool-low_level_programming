2d064d8cb68fea2f6c6deb55876f347da027f5bb
alfred <6666@holbertonstudents.com>
2023-07-04 09:54:41 -0700

    1..100

--- function_pointers/0-main.c
+++ function_pointers/0-main.c
@@ -6,8 +6,12 @@ void print_name(char *name, void (*f)(char *));
 
 int main(void)
 {
+	// Test case 1: print_name_as_is
 	print_name("Bob", print_name_as_is);
+
+	// Test case 2: print_name_uppercase
 	print_name("Bob Dylan", print_name_uppercase);
+
 	printf("\n");
 	return (0);
 }
