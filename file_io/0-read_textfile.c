e7f3b68c6af412816fc0f70e3cf35c1ff5eadd6a
afr117 <alfredo.figueroa07@yahoo.com>
2023-07-18 15:21:43 -0700

    1..100

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,43 +3,44 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed,
+ *         or 0 if there was an error.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, n;
-    char *buffer;
+    int fd;
+    ssize_t n;
+    char buffer[1];
 
     if (filename == NULL)
         return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
-
     fd = open(filename, O_RDONLY);
     if (fd == -1)
-    {
-        free(buffer);
         return (0);
+
+    n = read(fd, buffer, 1);
+    while (n > 0 && letters > 0)
+    {
+        if (write(STDOUT_FILENO, buffer, 1) == -1)
+        {
+            close(fd);
+            return (0);
+        }
+        n = read(fd, buffer, 1);
+        letters--;
     }
 
-    n = read(fd, buffer, letters);
-    if (n == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    n = write(STDOUT_FILENO, buffer, n);
-    if (n == -1 || (int)letters != n)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-
-    free(buffer);
     close(fd);
-    return (n);
+
+    if (n == -1 || letters > 0)
+        return (0);
+
+    return (letters);
 }
 
