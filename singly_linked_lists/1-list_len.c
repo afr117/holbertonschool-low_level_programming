6ad312b4faf6a3cdb79cd96b08084e1298c9a9f4
afr117 <alfredo.figueroa07@yahoo.com>
2023-07-18 09:35:47 -0700

    1..100

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 
 /**
- * list_len - Returns the number of elements in a linked list.
+ * list_len - Returns the number of elements in a linked list_t list.
  * @h: Pointer to the head of the list.
  *
  * Return: The number of elements in the list.
  */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+    size_t count = 0;
+    const list_t *current = h;
 
-	while (h != NULL)
-{
-	count++;
-	h = h->next;
+    while (current != NULL)
+    {
+        count++;
+        current = current->next;
+    }
+
+    return count;
 }
 
-	return (count);
-}
