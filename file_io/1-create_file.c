f58303d85a274289fd013e7e1418963b7a2049e6
afr117 <alfredo.figueroa07@yahoo.com>
2023-07-18 18:50:47 -0700

    1..100

---
+++ file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * create_file - Creates a file and writes text content to it
+ * @filename: Name of the file to create
+ * @text_content: Null-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, res, text_len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		/* Calculate the length of the text content */
+		while (text_content[text_len] != '\0')
+			text_len++;
+
+		/* Write the text content to the file */
+		res = write(fd, text_content, text_len);
+		if (res == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
+
