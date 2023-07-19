8e1cf59198a2bb0c35becdb9af1b11e56cbe2513
afr117 <alfredo.figueroa07@yahoo.com>
2023-07-18 18:36:32 -0700

    1..100

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,43 +5,43 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    size_t total = 0;
-    char *buffer;
-    ssize_t n;
+	int fd;
+	size_t total = 0;
+	char *buffer;
+	ssize_t n;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * (letters + 1));
-    if (buffer == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    while ((n = read(fd, buffer, letters)) > 0)
-    {
-        if (write(STDOUT_FILENO, buffer, n) != n)
-        {
-            free(buffer);
-            close(fd);
-            return (0);
-        }
+	while ((n = read(fd, buffer, letters)) > 0)
+	{
+		if (write(STDOUT_FILENO, buffer, n) != n)
+		{
+			free(buffer);
+			close(fd);
+			return (0);
+		}
 
-        total += n;
+		total += n;
 
-        if (total >= letters)
-            break;
-    }
+		if (total >= letters)
+			break;
+	}
 
-    close(fd);
-    free(buffer);
+	close(fd);
+	free(buffer);
 
-    return (total);
+	return (total);
 }
 
