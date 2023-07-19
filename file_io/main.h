4a63c76dd5b0783271b905f90ff63cf7034c4fbe
afr117 <alfredo.figueroa07@yahoo.com>
2023-07-18 18:35:28 -0700

    1..100

--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed, or 0 if an error occurs.
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* MAIN_H */
