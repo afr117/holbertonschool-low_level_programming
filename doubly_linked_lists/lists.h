eab592e6322192f1668c926c731bf49726c9c961
afr117 <alfredo.figueroa07@yahoo.com>
2023-07-22 10:16:17 -0700

    1..1000

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -20,13 +20,30 @@ typedef struct dlistint_s
 
 /* Function to print all the elements of a dlistint_t list */
 size_t print_dlistint(const dlistint_t *h);
+
+/* Function to get the number of elements in a dlistint_t list */
 size_t dlistint_len(const dlistint_t *h);
+
+/* Function to add a new node at the beginning of a dlistint_t list */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
+/* Function to add a new node at the end of a dlistint_t list */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+
+/* Function to free a dlistint_t list */
 void free_dlistint(dlistint_t *head);
+
+/* Function to get a node at a given index in a dlistint_t list */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+
+/* Function to calculate the sum of all integers stored in a dlistint_t list */
 int sum_dlistint(dlistint_t *head);
+
+/* Function to insert a new node at a given index in a dlistint_t list */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+
+/* Function to delete the node at a given index in a dlistint_t list */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif /* LISTS_H */
+
