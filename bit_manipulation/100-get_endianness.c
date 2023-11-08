7b776ecffff612fbd557c1f9056905b63c39600b
afr117 <alfredo.figueroa07@yahoo.com>
2023-11-07 16:01:40 -0800

    task 0-8 minus 7

---
+++ bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - Function to check the endianness of the system.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+    unsigned int x = 1;
+    char *c = (char *)&x;
+
+    /* If the first byte (lowest address) is 1, it's little-endian. */
+    return (*c);
+}
